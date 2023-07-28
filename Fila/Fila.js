e335c191b52ce6cefded878d71875d670e9475c6
NunjiSwoo <renanoliveira30@hotmail.com>
2023-07-27 21:51:12 -0300

    Fila

---
+++ Fila/Fila.js
@@ -0,0 +1,15 @@
+/*  Fila
+*   FIFO - First In First Out
+*   LIFO - Last In First Out
+*/
+
+var nomes = [];
+
+nomes. push('Maia', 'Rafão', 'Zangado', 'Tubarão');
+
+console.log(nomes);
+
+let proximo = nomes.shift();
+console.log('Próximo cliente: ', proximo)
+
+console.log(nomes);
